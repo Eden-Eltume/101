594da46ace2ef981060fb5c235cef1c0415c5a6a
Eden-Eltume <IEltume@gmail.com>
2017-01-19 12:17:04 -0500

    reading documentation 1

---
+++ Programming_Foundations/Lesson_4_Ruby_Collections/reading_documentation1.rb
@@ -0,0 +1,36 @@
+# Ex 1
+# Ruby-Doc.org
+
+# Ex 2
+ # Select the "control expressions" link.
+
+# Ex 3
+# nil, unless break is used, in which case the value passed to break is used.
+
+# Ex 4
+# nil if no arguments are passed to break, or the value of the argument if an argument is given.
+result = while true
+  break
+end
+p result
+
+# Ex 5
+# by using underscore to separate the tens, hundreds, thousands, place etc
+
+# Ex 6
+:Eden
+
+# Ex 7
+# When the method defines default arguments you do not need to supply all the arguments to the method.
+# Ruby will fill in the missing arguments in-order.
+[4, 5, 3, 6]
+
+# Ex 8
+# http://ruby-doc.org/core-2.4.0/String.html
+
+# Ex 9
+# rjust method
+
+# Ex 10
+# File::path is a class method, while File#path is an instance method.
+# Class methods are called on the class, while instance methods are called on objects.
