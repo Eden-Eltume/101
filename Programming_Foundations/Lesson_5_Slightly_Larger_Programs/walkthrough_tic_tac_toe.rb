36bc74a5b4ad83e9a51b2ec553c82c2bc9472ed8
Eden-Eltume <IEltume@gmail.com>
2017-01-27 23:27:34 -0500

    finished walkthrough tic-tac-toe

--- Programming_Foundations/Lesson_5_Slightly_Larger_Programs/walkthrough_tic_tac_toe.rb
+++ Programming_Foundations/Lesson_5_Slightly_Larger_Programs/walkthrough_tic_tac_toe.rb
@@ -9,17 +9,19 @@ def prompt(msg)
 end
 
 def display_board(brd)
-puts "     |     |     "
-puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}   "
-puts "     |     |     "
-puts "-----+-----+-----"
-puts "     |     |     "
-puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}   "
-puts "     |     |     "
-puts "-----+-----+-----"
-puts "     |     |     "
-puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}   "
-puts "     |     |     "
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts "     |     |     "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}   "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}   "
+  puts "     |     |     "
+  puts "-----+-----+-----"
+  puts "     |     |     "
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}   "
+  puts "     |     |     "
 end
 
 def initialize_board
@@ -56,19 +58,53 @@ def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-def someone_won?(board)
-  false
+def someone_won?(brd)
+  !!detect_winner(brd)
 end
 
-board = initialize_board
-display_board(board)
+def detect_winner(brd)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + # rows
+                  [[1,4,7], [2,5,8], [3,6,9]] + # cols
+                  [[1,5,9], [3,5,7]]            # diags
+
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif brd[line[0]] == COMPUTER_MARKER &&
+        brd[line[1]] == COMPUTER_MARKER &&
+        brd[line[2]] == COMPUTER_MARKER
+        return 'Computer'
+     end
+  end
+  nil
+end
 
 loop do
-  player_places_piece!(board)
-  computer_places_piece!(board)
-  puts board.inspect
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
-  break if someone_won?(board) || board_full?(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again?"
+  answer = gets.chomp.strip
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
+prompt "Thanks for playing tic-tac-toe. Goodbye!"
