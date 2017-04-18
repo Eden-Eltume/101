5ff10ccd17fc4c1e64b81b9ad8a31d3c427d7e07
Eden-Eltume <IEltume@gmail.com>
2017-04-18 14:09:15 -0400

    Easy 9 Ex 6-10

---
+++ Exercises/101_109_Small_Problems2/Easy9/ex8.rb
@@ -0,0 +1,15 @@
+def sequence(count, num)
+  arr = []
+  counter = 1
+  loop do
+    break if counter == count + 1
+    arr << num * counter
+    counter += 1
+  end
+  arr
+end
+
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
