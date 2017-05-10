c0d66b8fd7d56d109663b7a019e908dd06095b75
Eden-Eltume <IEltume@gmail.com>
2017-05-10 18:13:43 -0400

    Medium 1 Ex5

--- Exercises/101_109_Small_Problems2/Medium1/ex5.rb
+++ Exercises/101_109_Small_Problems2/Medium1/ex5.rb
@@ -17,3 +17,18 @@ def diamond(num)
 end
 
 diamond(9)
+
+# My other solution
+=begin
+def diamond(n)
+  (n + 1).times do |i|
+    str = ("*" * i).to_s
+    puts str.center(n) unless i.even?
+  end
+
+  (n-1).downto(1) do |i|
+    str2 = ("*" * i).to_s
+    puts str2.center(n) unless i.even?
+  end
+end
+=end
