76ca0f19bf7f9176471f2849563105f7d7de1eff
Eden Eltume <IEltume@gmail.com>
2018-10-08 22:08:50 -0400

    revise rps_bonus_features

--- Programming_Foundations2/Lesson_2_Small_Programs/rps_bonus_features.rb
+++ Programming_Foundations2/Lesson_2_Small_Programs/rps_bonus_features.rb
@@ -1,17 +1,17 @@
 VALID_CHOICES = {
-  "rock": 'r',
-  "paper": 'p',
-  "scissors": 's',
-  "lizard": 'l',
-  "spock": 'k'
+  'rock' => 'r',
+  'paper' => 'p',
+  'scissors' => 's',
+  'lizard' => 'l',
+  'spock' => 'k'
 }
 
 WINNING_SCENARIOS = {
-  "rock": ['scissors', 'lizard'],
-  "paper": ['rock', 'spock'],
-  "scissors": ['paper', 'lizard'],
-  "lizard": ['spock', 'paper'],
-  "spock": ['scissors', 'rock']
+  'rock' => ['scissors', 'lizard'],
+  'paper' => ['rock', 'spock'],
+  'scissors' => ['paper', 'lizard'],
+  'lizard' => ['spock', 'paper'],
+  'spock' => ['scissors', 'rock']
 }
 
 NUMBER_OF_WINS = 5
@@ -20,8 +20,8 @@ def prompt(message)
   puts("=> #{message}")
 end
 
-def win?(first, second)
-  WINNING_SCENARIOS.values_at(first).flatten!.include?(second.to_s)
+def win?(choice1, choice2)
+  WINNING_SCENARIOS.values_at(choice1).flatten!.include?(choice2)
 end
 
 def tell_who_won(player, computer)
@@ -44,15 +44,19 @@ def display_results(player, computer)
   end
 end
 
-loop do
+def clear_screen
   system('clear' || 'clr')
+end
+
+loop do
+  clear_screen
+
   players_score = 0
   computers_score = 0
   ties = 0
   loop do
     choice = ''
     loop do
-      puts ''
       prompt("Welcome! Choose:")
       VALID_CHOICES.each do |valid_choice, letter|
         prompt("#{letter} for #{valid_choice}")
@@ -82,11 +86,7 @@ loop do
       ties += 1
     end
 
-    system('clear' || 'clr')
-
-    puts "Player: #{players_score}"
-    puts "Computer: #{computers_score}"
-    puts "Ties: #{ties}"
+    clear_screen
 
     if players_score >= NUMBER_OF_WINS
       puts "You won #{NUMBER_OF_WINS} times with only #{ties} tie(s)!"
@@ -95,9 +95,14 @@ loop do
       puts "The computer won #{NUMBER_OF_WINS} times with only #{ties} tie(s)!"
       break
     end
+
+    puts "Player: #{players_score}"
+    puts "Computer: #{computers_score}"
+    puts "Ties: #{ties}"
+    puts ""
   end
 
-  puts "Do you want to play again?"
+  puts "Do you want to play again? (Y for yes)"
   answer = gets.chomp
   break unless answer.downcase == 'y' || answer.downcase == 'yes'
 end
